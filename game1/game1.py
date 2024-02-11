25a64e813df086de66cc70a7dec3f847c1a505d3
IVO JONES <phuongsepfg@gmail.com>
2024-02-11 07:19:48 +0700

    .

--- game1/game1.py
+++ game1/game1.py
@@ -3,10 +3,10 @@ import pygame
 pygame.init( )
 
 #Hang trong luc
-p = 0.2 
+p = 0.2
 bird_y = 0 #vi tri bat len 
 score=0 #Khoi tao diem
-
+highScore = 0
 game_play = True
 
 #Tieu de va icon game
@@ -17,6 +17,7 @@ pygame.display.set_icon(icon)
 #Set background cho game
 bg = pygame.image.load(r'E:\firstpythongame\game1\assets\background-night.png')
 bg=pygame.transform.scale2x(bg)
+
 #Set a float for 
 fl= pygame.image.load(r'E:\firstpythongame\game1\assets\floor.png')
 fl=pygame.transform.scale2x(fl)
@@ -25,10 +26,11 @@ fl_x=0
 #Set a bird for a game
 bird = pygame.image.load(r'E:\firstpythongame\game1\assets\yellowbird-midflap.png')
 bird = pygame.transform.scale2x(bird)
-bird_hcn = bird.get_rect(center=(100,386))
+bird_hcn = bird.get_rect(center=(100,296))
+
 #Ham check va cham
 def check_vc():
-    if bird_hcn.bottom>=700 or bird_hcn.top <= -75:
+    if bird_hcn.bottom>=850 or bird_hcn.top <= -75:
         return False
     else:
         return True
@@ -45,9 +47,15 @@ screen = pygame.display.set_mode((432,768))  #Kich thuoc man
 #score
 game_font = pygame.font.Font(r'E:\firstpythongame\game1\04B_19.TTF',40)
 def score_view():
-    score_f = game_font.render(str(int(score)),True,(255,255,255))
-    score_hcn = score_f.get_rect(center=(200,100))
-    screen.blit(score_f,score_hcn)
+    if game_play: 
+        score_f = game_font.render(f'Score: {(int(score))}',True,(255,255,255))
+        score_hcn = score_f.get_rect(center=(200,100))
+        screen.blit(score_f,score_hcn)
+    if game_play==False:
+        #High Score
+        highScore = game_font.render(f'High Score: {(int(score))}',True,(255,255,255))
+        highScore_hcn = highScore.get_rect(center=(200,100))
+        screen.blit(highScore,highScore_hcn)
 #Vong lap(giu cua so terminal luon chay khi chua co ngoai le)
 running = True
 while running:
@@ -55,8 +63,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type== pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                bird_y = -10      
+            #if game allway true 
+            if event.key == pygame.K_SPACE and game_play:
+                bird_y = -10
+                #if u touch a float or highest sky
+            if event.key == pygame.K_SPACE and game_play == False:
+                game_play = True
+                bird_y = 0
+                score = 0 
+                bird_hcn.center=(100,296)
     screen.blit(bg,(0,0))
     fl_x-=0.5
     screen.blit(fl,(fl_x,700))
@@ -70,8 +85,11 @@ while running:
         bird_y += p
         bird_hcn.centery += bird_y
         score+=0.01
+        if score > highScore:
+            highScore = score
         score_view()
         game_play = check_vc()
     else:
         screen.blit(screen_out,screen_out_hcn)
+        score_view()
     pygame.display.update()
